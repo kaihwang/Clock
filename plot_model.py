ff85947572d2d0aa9630766ed1baf94c6f875899
Kai Hwang <kai.hwang@gmail.com>
2021-06-21 15:06:27 -0500

    plot updates.

--- plot_model.py
+++ plot_model.py
@@ -24,6 +24,8 @@ rdata = pyreadr.read_r(datapath + 'tf_entropy_random_slopes_encoding_models.rds'
 df = rdata[None]
 # cut it down to only include sensor data
 sdf = df.loc[df.group=='sensor']
+sdf = sdf.loc[sdf.effect=='ran_coefs']
+sdf = sdf.loc[sdf.alignment=='rt']
 sdf.Freq = sdf.Freq.astype('float')
 
 def create_param_tfr(sdf, term):
@@ -56,7 +58,45 @@ entropy_change_tfr.plot_joint(mode='mean', timefreqs=[(-1, 10), (1, 20)], picks=
 entropy_change_tfr.plot_topomap(ch_type='grad', tmin=-1, tmax=-0.8, fmin=15, fmax=40, title='Beta at time -1 to -.8')
 
 
+reward_tfr = create_param_tfr(sdf, 'reward_t')
+reward_tfr.plot_joint(mode='mean', timefreqs=[(-1, 10),(0.6, 24), (1, 20)], picks='grad')
+reward_tfr.plot_topomap(ch_type='grad', tmin=0.4, tmax=0.8, fmin=15, fmax=30, title='Beta at time 0.4 to 0.8')
 
+entropy_wi_tfr = create_param_tfr(sdf, 'v_entropy_wi')
+
+fdf = df.loc[pd.isna(df.group)]
+fdf = fdf.loc[fdf.alignment=='rt']
+fdf.Freq = fdf.Freq.astype('float')
+
+def create_param_fixed_tfr(sdf, term):
+    # creat TFR epoch object for plotting. Use the "info" in this file for measurement info
+    template_TFR = mne.time_frequency.read_tfrs(datapath + 'Group/group_feedback_power-tfr.h5')[0]
+    # the data array in this template is 306 ch by 20 freq by 464 time
+
+    # create custom tfr data array
+    tdf=sdf.loc[sdf.term==term]
+    time = tdf.Time.unique() #what is the diff between "Time" and "t" in the dataframe?
+    freq = tdf.Freq.unique()
+    new_data = np.zeros((306, len(freq), len(time)))
+
+    # now plut in real stats into the dataframe
+    for index, row in tdf.iterrows():
+        t = row.Time
+        f = row.Freq
+        #ch = 'MEG'+row.level
+        #ch_idx = mne.pick_channels(template_TFR.ch_names, [ch])
+        new_data[:, np.where(freq==f)[0], np.where(time==t)[0]] = row.estimate
+    new_tfr = mne.time_frequency.AverageTFR(template_TFR.info, new_data, time, freq, 1)
+
+    return new_tfr
+
+
+
+entropy_wi_fixed_tfr = create_param_fixed_tfr(fdf, 'v_entropy_wi')
+
+
+
+### note, check y axis, in log space?
 
 
 # end
